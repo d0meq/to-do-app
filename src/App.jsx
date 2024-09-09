672ed59f955e9423274060f0f20e6777c34ef6e3
d0meq <moutiie11@gmail.com>
2024-09-09 18:48:34 +0200

    Clearing app.jsx pt.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,6 @@
 import ToDo from './ToDo.jsx';
 
-
 function App() {
-  // const fruits = [{id: 1, name:"apple", calories: 95},
-  //                 {id: 2, name:"orange", calories: 45}, 
-  //                 {id: 3, name: "bannana", calories: 105}, 
-  //                 {id: 4, name: "coconut", calories: 159}, 
-  //                 {id: 5, name: "pineapple", calories: 37}]
-
-  // const vegetables = [{id: 6, name:"potatos", calories: 110},
-  //                     {id: 7, name:"celery", calories: 15}, 
-  //                     {id: 8, name: "carrots", calories: 63}, 
-  //                     {id: 9, name: "broccolu", calories: 50}, 
-  //                     {id: 10, name: "corn", calories: 63}]
-  
   return(
     <>
       <ToDo />
